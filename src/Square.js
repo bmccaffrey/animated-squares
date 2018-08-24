50ef5d54022e3204e2b7e841cbee386d1ca336a0
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-24 14:57:35 -0500

    create Square component

---
+++ src/Square.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './square.css';
+
+export default class Square extends Component {
+  render() {
+    return (
+      <div>
+        <div class="square" />
+      </div>
+    );
+  }
+}

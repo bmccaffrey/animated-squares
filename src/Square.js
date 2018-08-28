9269dfc436320ad8907eb2a6fb83f2d53ac67872
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-28 10:49:06 -0500

    create styled component for animation

--- src/Square.js
+++ src/Square.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import './square.css';
+import styled, { keyframes } from 'styled-components';
+// import './square.css';
 
 export default class Square extends Component {
   render() {
     return (
       <div>
-        <div class="square" />
+        <Stuff />
+        {/* <div class="square" /> */}
       </div>
     );
   }
 }
+
+const Rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(135deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+`;
+
+const Stuff = styled.div`
+  height: 50px;
+  width: 50px;
+  background: red;
+  border-radius: 10px;
+  :hover {
+    background: blue;
+    animation-duration: 1s;
+    animation-name: ${Rotate};
+  }
+`;

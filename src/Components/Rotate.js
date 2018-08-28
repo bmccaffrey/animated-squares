2fa07501ac28dafd30cd36f7856483eb5617fb5b
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-28 11:12:18 -0500

    add animation to library

---
+++ src/Components/Rotate.js
@@ -0,0 +1,13 @@
+import styled, { keyframes } from 'styled-components';
+
+export const Rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(135deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+`;
